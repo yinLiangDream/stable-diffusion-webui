280057e26df0b125ac00254f400407395da5e62e
梁音 <s17chucc@163.com>
2023-03-29 22:57:42 +0800

    store

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\s17ch\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--medvram --autolaunch --deepdanbooru --xformers
 
 call webui.bat
